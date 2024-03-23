40f61eeca2a859c45ba971a55044e0009d83336b
elozsofi <elo.zsofi@gmail.com>
2024-03-23 20:26:32 +0100

    Pontok és egyenesek

--- hf_1/Programs/Skeleton/Skeleton/Skeleton.cpp
+++ hf_1/Programs/Skeleton/Skeleton/Skeleton.cpp
@@ -18,8 +18,8 @@
 //
 // NYILATKOZAT
 // ---------------------------------------------------------------------------------------------
-// Nev    : Élõ Zsófia Anna		
-// Neptun : MAGH42
+// Nev    : 	
+// Neptun : 
 // ---------------------------------------------------------------------------------------------
 // ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
 // mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
